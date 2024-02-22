e4d734f3ce2d72bcbc0de5cd8f0c55c78978a32d
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-02-22 23:09:32 +0400

    ex

--- examples/cs/interop-non-transpilable-code/output/index.cs
+++ examples/cs/interop-non-transpilable-code/output/index.cs
@@ -5,7 +5,7 @@ class MyClass
     public virtual void mainFeature(object message)
     {
         object ar = new List<object>() {1, 2, 3};
-        ((IList<object>)ar).Reverse();
+        ar = (ar as IList<object>).Reverse();
         Console.WriteLine(ar);
         Console.WriteLine(add("Hello! I\'m inside main class:", message));
         nonTranspilableFeature(); // invoke non-transpilable code here normally
